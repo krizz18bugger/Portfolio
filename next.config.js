d9c0fef5f8e465bc0f944b63a2e01580d1044014
krizz18bugger <moorthi18krishna@gmail.com>
2026-05-11 21:29:20 +0530

    fix: remove incorrect outputFileTracingRoot to resolve Vercel deployment error

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
 
 const nextConfig = {
   reactStrictMode: true,
-  output: undefined,
-  outputFileTracingRoot: path.join(__dirname, '../../'),
   images: {
     domains: [],
   },
